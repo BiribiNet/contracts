1ee5a3e2b087b1562b2a096fdac1802e9df8361b
BiribiNet <info@biribi.net>
2025-09-08 18:08:29 +0200

    refactor: remove ProfitDistributed event from StakedBRB contract to enhance event clarity and reduce redundancy

--- contracts/StakedBRB.sol
+++ contracts/StakedBRB.sol
@@ -104,7 +104,6 @@ contract StakedBRB is ERC4626Upgradeable, AccessControlUpgradeable, UUPSUpgradea
     
     // Events
     event BetPlaced(address user, uint256 amount, bytes data);
-    event ProfitDistributed(uint256 amount);
     event ProtocolFeeCollected(uint256 amount);
     event ProtocolFeeRateUpdated(uint256 newFee);
     event ProtocolFeeRecipientUpdated(address newRecipient);
